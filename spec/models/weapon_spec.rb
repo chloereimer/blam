679ec5a23b05a2b190c223b259177b3b6d033af2
Chloe Reimer <chloe.reimer@prodigygame.com>
2021-06-26 09:26:13 -0400

    removes :focus in weapon model spec

--- spec/models/weapon_spec.rb
+++ spec/models/weapon_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Weapon, type: :model do
-  it "has a valid factory", :focus do
+  it "has a valid factory" do
     expect(build(described_class_symbol)).to be_valid
   end
 end
